8c112219b9c394c24c8372d2d0c36dd3bd165316
reid <r.scoggin@gmail.com>
2022-02-08 11:33:36 -0600

    Stripped out btc RPC server, added basicauth

--- start.sh
+++ start.sh
@@ -5,9 +5,11 @@ export ELECTRUM_IP=$ELECTRUM_IP
 export ELECTRUM_PORT=$ELECTRUM_PORT
 export BITCOIN_RPC_AUTH=$BITCOIN_RPC_AUTH
 export BITCOIN_RPC_PASS=$BITCOIN_RPC_PASS
+export APP_PASSWORD=$APP_PASSWORD
 export PROXY_PORT=50002
 cp -r /tmp/app/ /app
+mkdir -p /etc/apache2
+htpasswd -b -c /etc/apache2/.htpasswd umbrel $APP_PASSWORD
 nginx -c /etc/nginx/conf.d/nginx.conf
 cd /app
 env FLASK_APP=app.py python3 -m flask run --host=0.0.0.0 &
-node /src/server.js >> /proc/1/fd/1
