566d985dfd82616c8d0d14ecfe2b34dd3cc245d3
Amadeo Bellotti <amadeo.bellotti@gmail.com>
2022-06-02 08:57:32 -0400

    updated bianries to v1.9

--- install-urbit.sh
+++ install-urbit.sh
@@ -4,8 +4,8 @@ DEVICE_ARCH=$(uname -m)
 if [[ $DEVICE_ARCH == "aarch64" ]]; then
   mkdir -p /urbit/binary
   cd /urbit/binary/
-  wget https://github.com/botter-nidnul/urbit/releases/download/urbit-v1.8-aarch64/urbit-v1.8-aarch64-linux.tgz
-  tar zxvf ./urbit-v1.8-aarch64-linux.tgz --strip=1
+  wget https://github.com/mopfel-winrux/urbit-umbrel/releases/download/urbit-v1.9/urbit-v1.9-aarch64-linux.tgz
+  tar zxvf ./urbit-v1.9-aarch64-linux.tgz --strip=1
   mv /urbit/binary/urbit* /usr/sbin/
 
 elif [[ $DEVICE_ARCH == "x86_64" ]]; then
