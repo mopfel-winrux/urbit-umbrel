cf2b7111bea42642232035bca26984454aef6650
reid <reid@scogg.in>
2022-09-04 12:27:35 -0500

    1.10 binary

--- install-urbit.sh
+++ install-urbit.sh
@@ -4,9 +4,9 @@ DEVICE_ARCH=$(uname -m)
 if [[ $DEVICE_ARCH == "aarch64" ]]; then
   mkdir -p /urbit/binary
   cd /urbit/binary/
-  wget https://github.com/mopfel-winrux/urbit-umbrel/releases/download/urbit-v1.9/urbit-v1.9-aarch64-linux.tgz
+  wget https://github.com/botter-nidnul/urbit/releases/download/urbit-v1.10-aarch64-linux/urbit-v1.10-aarch64-linux.tgz
   tar zxvf ./urbit-v1.9-aarch64-linux.tgz --strip=1
-  mv /urbit/binary/urbit* /usr/sbin/
+  mv /urbit/binary/urbit /usr/sbin/
 
 elif [[ $DEVICE_ARCH == "x86_64" ]]; then
   mkdir -p /urbit/binary
