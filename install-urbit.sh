1f8cf980b73aaadad668b67c916c19b35382b1cf
Amadeo Bellotti <amadeo.bellotti@gmail.com>
2023-01-04 15:15:40 -0500

    changed to just be v1.15

--- install-urbit.sh
+++ install-urbit.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 DEVICE_ARCH=$(uname -m)
+VERSION="v1.15"
 mkdir -p /urbit/binary
 cd /urbit/binary/
 if [[ $DEVICE_ARCH == "aarch64" ]]; then
-  curl -L https://urbit.org/install/linux-aarch64/latest | tar xzk --strip=1
+  curl -L https://github.com/urbit/urbit/releases/download/urbit-$VERSION/linux-aarch64.tgz | tar xzk --strip=1
 elif [[ $DEVICE_ARCH == "x86_64" ]]; then
-  curl -L https://urbit.org/install/linux64/latest | tar xzk --strip=1
+  curl -L https://github.com/urbit/urbit/releases/download/urbit-$VERSION/linux64.tgz | tar xzk --strip=1
 fi
 mv /urbit/binary/urbit /usr/sbin/
