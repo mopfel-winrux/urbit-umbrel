5331155e4c57e0cf3e1db87cbab2e1764700f910
reid <yapishu@users.noreply.github.com>
2023-01-15 18:48:28 -0600

    Fix wrong filenames

--- install-urbit.sh
+++ install-urbit.sh
@@ -6,8 +6,8 @@ mkdir -p /urbit/binary
 cd /urbit/binary/
 if [[ $DEVICE_ARCH == "aarch64" ]]; then
   curl -L https://github.com/urbit/vere/releases/download/vere-$VERSION/linux-aarch64.tgz | tar xzk
-  mv /urbit/binary/vere-${VERSION}-linux-x86_64 /usr/sbin/urbit
+  mv /urbit/binary/vere-${VERSION}-linux-aarch64 /usr/sbin/urbit
 elif [[ $DEVICE_ARCH == "x86_64" ]]; then
   curl -L https://github.com/urbit/vere/releases/download/vere-$VERSION/linux-x86_64.tgz | tar xzk
-  mv /urbit/binary/vere-${VERSION}-linux-aarch64 /usr/sbin/urbit
+  mv /urbit/binary/vere-${VERSION}-linux-x86_64 /usr/sbin/urbit
 fi
