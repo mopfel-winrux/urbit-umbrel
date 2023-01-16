12293af01461201bd0f456b2d72a648ed53c29e8
reid <yapishu@users.noreply.github.com>
2023-01-15 18:37:17 -0600

    Fix binary extraction to match new naming scheme

--- install-urbit.sh
+++ install-urbit.sh
@@ -5,8 +5,9 @@ VERSION="v1.16"
 mkdir -p /urbit/binary
 cd /urbit/binary/
 if [[ $DEVICE_ARCH == "aarch64" ]]; then
-  curl -L https://github.com/urbit/vere/releases/download/vere-$VERSION/linux-aarch64.tgz | tar xzk --strip=1
+  curl -L https://github.com/urbit/vere/releases/download/vere-$VERSION/linux-aarch64.tgz | tar xzk
+  mv /urbit/binary/vere-${VERSION}-linux-x86_64 /usr/sbin/urbit
 elif [[ $DEVICE_ARCH == "x86_64" ]]; then
-  curl -L https://github.com/urbit/vere/releases/download/vere-$VERSION/linux-x86_64.tgz | tar xzk --strip=1
+  curl -L https://github.com/urbit/vere/releases/download/vere-$VERSION/linux-x86_64.tgz | tar xzk
+  mv /urbit/binary/vere-${VERSION}-linux-aarch64 /usr/sbin/urbit
 fi
-mv /urbit/binary/urbit /usr/sbin/
